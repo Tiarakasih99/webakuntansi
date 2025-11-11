f705c3991e5e21e11345429ad6cea9c07013a72f
ellasafitri0910-web <ellasafitri0910@gmail.com>
2025-11-11 22:11:27 +0700

    membuat halaman laoran laba rugi

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@ Route::view('/neraca-ses', 'pages.transaksi.neraca_saldo_pen')->name('neraca_ses
 Route::view('/laba-rugi', 'pages.laporan.labarugi')->name('laba_rugi.index');
 Route::view('/posisi-keuangan', 'pages.laporan.posisi_keuangan')->name('posisi_keuangan.index');
 Route::view('/perubahan-modal', 'pages.laporan.perubahan_modal')->name('perubahan_modal.index');
+
+use App\Http\Controllers\DashboardController;
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+use App\Http\Controllers\LabaRugiController;
+
+Route::get('/laporan/laba-rugi', [LabaRugiController::class, 'index'])->name('laporan.labarugi.index');
+Route::post('/laporan/laba-rugi/generate', [LabaRugiController::class, 'generate'])->name('laporan.labarugi.generate');
